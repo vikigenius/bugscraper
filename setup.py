11166fd576526ea9f827a4c5ad7feb7b9e12eefe
vikigenius <v9balasu@uwaterloo.ca>
2019-11-07 03:01:43 -0500

    Updated requirements in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0', 'tqdm>=4.0', 'requests>=2.22']
 
 setup_requirements = ['pytest-runner', ]
 
