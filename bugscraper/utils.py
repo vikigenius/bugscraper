f2015fb7e22a492d36f0ec2561c5acfe3873733a
vikigenius <v9balasu@uwaterloo.ca>
2019-12-22 22:12:33 -0500

    Filtering WIP

--- bugscraper/utils.py
+++ bugscraper/utils.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 import json
+import logging
 import pandas as pd
 from pathlib import Path
+
+
+logger = logging.getLogger('bugscraper')
 
 
 def divide_chunks(l, n):
@@ -15,12 +19,15 @@ def get_dataframe(save_path: Path):
     years = set()
 
     # Reading the metadata file to get years
+    logger.debug('Reading metadata file to get years')
     with open(Path(save_path, 'bug_metadata.jsonl')) as mf:
         for line in mf:
             years.add(json.loads(line)['year'])
 
+    logger.debug(f'Available Years: {years}')
     dflist = []
     for year in years:
+        logger.debug(f'Reading Year: {year}')
         with open(Path(save_path, str(year) + '.jsonl')) as bf:
             metalist = [json.loads(line) for line in bf]
             dflist.append(pd.DataFrame(metalist))
